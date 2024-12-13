be400cfd524e3ebe89ded1c845681767b04a9a72
Bryan11L <bryansimoon@gmail.com>
2024-12-13 14:46:42 -0500

    Segundo send

--- app/src/main/java/dev/bryan/larrea/MainActivity.kt
+++ app/src/main/java/dev/bryan/larrea/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScrollModifierNode
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.bryan.larrea.ui.theme.BryanAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +21,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BryanAPPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Saludo()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Saludo(){
+    Column  (modifier = Modifier.padding(vertical = 50.dp, horizontal = 50.dp)
+    ){
+
+        Text (
+            text = "Hola",
+
+        )
+        Text (
+            text = "Mi name es BRYAN",
+
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BryanAPPTheme {
-        Greeting("Android")
-    }
-}
+
